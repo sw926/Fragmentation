457c08471ff756dd3db71d289c22ec57dfb563cd
sunwei <sunw926@gmail.com>
2020-03-08 21:41:26 +0800

    convert to kotlin

--- fragmentation_swipeback/src/main/java/me/yokeyword/fragmentation_swipeback/core/SwipeBackActivityDelegate.kt
+++ fragmentation_swipeback/src/main/java/me/yokeyword/fragmentation_swipeback/core/SwipeBackActivityDelegate.kt
@@ -1,50 +1,40 @@
-package me.yokeyword.fragmentation_swipeback.core;
+package me.yokeyword.fragmentation_swipeback.core
 
-import android.graphics.Color;
-import android.graphics.drawable.ColorDrawable;
-import android.os.Bundle;
-import androidx.fragment.app.FragmentActivity;
-import android.view.ViewGroup;
-
-import me.yokeyword.fragmentation.ISupportActivity;
-import me.yokeyword.fragmentation.SwipeBackLayout;
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.os.Bundle
+import android.view.ViewGroup
+import androidx.fragment.app.FragmentActivity
+import me.yokeyword.fragmentation.ISupportActivity
+import me.yokeyword.fragmentation.SwipeBackLayout
+import me.yokeyword.fragmentation.SwipeBackLayout.EdgeLevel
 
 /**
  * Created by YoKey on 17/6/29.
  */
+class SwipeBackActivityDelegate(swipeBackActivity: ISwipeBackActivity?) {
+    private val mActivity: FragmentActivity
+    var swipeBackLayout: SwipeBackLayout? = null
+        private set
 
-public class SwipeBackActivityDelegate {
-    private FragmentActivity mActivity;
-    private SwipeBackLayout mSwipeBackLayout;
-
-    public SwipeBackActivityDelegate(ISwipeBackActivity swipeBackActivity) {
-        if (!(swipeBackActivity instanceof FragmentActivity) || !(swipeBackActivity instanceof ISupportActivity))
-            throw new RuntimeException("Must extends FragmentActivity/AppCompatActivity and implements ISupportActivity");
-        mActivity = (FragmentActivity) swipeBackActivity;
+    fun onCreate(savedInstanceState: Bundle?) {
+        onActivityCreate()
     }
 
-    public void onCreate(Bundle savedInstanceState) {
-        onActivityCreate();
+    fun onPostCreate(savedInstanceState: Bundle?) {
+        swipeBackLayout?.attachToActivity(mActivity)
     }
 
-    public void onPostCreate(Bundle savedInstanceState) {
-        mSwipeBackLayout.attachToActivity(mActivity);
+    fun setSwipeBackEnable(enable: Boolean) {
+        swipeBackLayout?.setEnableGesture(enable)
     }
 
-    public SwipeBackLayout getSwipeBackLayout() {
-        return mSwipeBackLayout;
+    fun setEdgeLevel(edgeLevel: EdgeLevel?) {
+        swipeBackLayout?.setEdgeLevel(edgeLevel)
     }
 
-    public void setSwipeBackEnable(boolean enable) {
-        mSwipeBackLayout.setEnableGesture(enable);
-    }
-
-    public void setEdgeLevel(SwipeBackLayout.EdgeLevel edgeLevel) {
-        mSwipeBackLayout.setEdgeLevel(edgeLevel);
-    }
-
-    public void setEdgeLevel(int widthPixel) {
-        mSwipeBackLayout.setEdgeLevel(widthPixel);
+    fun setEdgeLevel(widthPixel: Int) {
+        swipeBackLayout?.setEdgeLevel(widthPixel)
     }
 
     /**
@@ -52,15 +42,20 @@ public class SwipeBackActivityDelegate {
      *
      * @return true: Activity可以滑动退出, 并且总是优先;  false: Fragment优先滑动退出
      */
-    public boolean swipeBackPriority() {
-        return mActivity.getSupportFragmentManager().getBackStackEntryCount() <= 1;
+    fun swipeBackPriority(): Boolean {
+        return mActivity.supportFragmentManager.backStackEntryCount <= 1
     }
 
-    private void onActivityCreate() {
-        mActivity.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
-        mActivity.getWindow().getDecorView().setBackgroundColor(Color.TRANSPARENT);
-        mSwipeBackLayout = new SwipeBackLayout(mActivity);
-        ViewGroup.LayoutParams params = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
-        mSwipeBackLayout.setLayoutParams(params);
+    private fun onActivityCreate() {
+        mActivity.window.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        mActivity.window.decorView.setBackgroundColor(Color.TRANSPARENT)
+        swipeBackLayout = SwipeBackLayout(mActivity)
+        val params = ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT)
+        swipeBackLayout?.layoutParams = params
+    }
+
+    init {
+        if (swipeBackActivity !is FragmentActivity || swipeBackActivity !is ISupportActivity) throw RuntimeException("Must extends FragmentActivity/AppCompatActivity and implements ISupportActivity")
+        mActivity = swipeBackActivity
     }
 }
